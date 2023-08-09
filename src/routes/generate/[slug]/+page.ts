b4f39aa39a976eaec959c75495e83db33021efe7
Martin-Ukhanov <martin.ukhanov@gmail.com>
2023-08-08 21:00:52 -0400

    Change slug to lowercase

--- src/routes/generate/[slug]/+page.ts
+++ src/routes/generate/[slug]/+page.ts
@@ -1,10 +1,17 @@
 import { validColorPaletteSlug, slugToColorPalette } from '$lib/utils';
 import { error } from '@sveltejs/kit';
+import { goto } from '$app/navigation';
 import type { PageLoad } from './$types';
 
 export const load = (({ params }) => {
-	if (!validColorPaletteSlug(params.slug)) {
+	const { slug } = params;
+
+	if (!validColorPaletteSlug(slug)) {
 		throw error(404, { message: 'Page Not Found' });
+	}
+
+	if (/[A-Z]/.test(slug)) {
+		goto(`/generate/${slug.toLowerCase()}`);
 	}
 
 	return { colorPalette: slugToColorPalette(params.slug) };
