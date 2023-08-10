37d94bb920d7bc9aa85a2c633a98384f1b04435a
Martin-Ukhanov <martin.ukhanov@gmail.com>
2023-08-10 18:11:47 -0400

    Added transition for colors

--- src/lib/components/GenerateMenu.svelte
+++ src/lib/components/GenerateMenu.svelte
@@ -2,6 +2,9 @@
 	import { goto } from '$app/navigation';
 	import uniqid from 'uniqid';
 	import { generateColorScheme, colorSchemeToSlug, contrastingColor } from '$lib/utils';
+	import { slide, crossfade } from 'svelte/transition';
+	import { flip } from 'svelte/animate';
+	import { cubicOut } from 'svelte/easing';
 
 	type Color = {
 		id: string;
@@ -102,37 +105,56 @@
 	function toggleLockedColor(index: number): void {
 		colorSchemes[colorSchemeIndex][index].locked = !colorSchemes[colorSchemeIndex][index].locked;
 	}
+
+	export const [send, receive] = crossfade({
+		fallback(node) {
+			const style = getComputedStyle(node);
+			const transform = style.transform === 'none' ? '' : style.transform;
+
+			return {
+				duration: 150,
+				easing: cubicOut,
+				css: (t) => `
+				transform: ${transform} scale(${t});
+				opacity: ${t}
+			`
+			};
+		}
+	});
 </script>
 
-<menu
-	class="fixed bottom-4 left-4 right-4 h-64 p-4 flex gap-4 border-3 rounded-lg bg-white border-black"
->
-	<button class="button" on:click={addColorScheme}>
-		<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="w-8 fill-black">
-			<path
-				d="M10 11H7.101l.001-.009a4.956 4.956 0 0 1 .752-1.787 5.054 5.054 0 0 1 2.2-1.811c.302-.128.617-.226.938-.291a5.078 5.078 0 0 1 2.018 0 4.978 4.978 0 0 1 2.525 1.361l1.416-1.412a7.036 7.036 0 0 0-2.224-1.501 6.921 6.921 0 0 0-1.315-.408 7.079 7.079 0 0 0-2.819 0 6.94 6.94 0 0 0-1.316.409 7.04 7.04 0 0 0-3.08 2.534 6.978 6.978 0 0 0-1.054 2.505c-.028.135-.043.273-.063.41H2l4 4 4-4zm4 2h2.899l-.001.008a4.976 4.976 0 0 1-2.103 3.138 4.943 4.943 0 0 1-1.787.752 5.073 5.073 0 0 1-2.017 0 4.956 4.956 0 0 1-1.787-.752 5.072 5.072 0 0 1-.74-.61L7.05 16.95a7.032 7.032 0 0 0 2.225 1.5c.424.18.867.317 1.315.408a7.07 7.07 0 0 0 2.818 0 7.031 7.031 0 0 0 4.395-2.945 6.974 6.974 0 0 0 1.053-2.503c.027-.135.043-.273.063-.41H22l-4-4-4 4z"
-			/>
-		</svg>
-	</button>
-	<button class="button" on:click={undoColorScheme}>
-		<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="w-8 fill-black">
-			<path
-				d="M9 10h6c1.654 0 3 1.346 3 3s-1.346 3-3 3h-3v2h3c2.757 0 5-2.243 5-5s-2.243-5-5-5H9V5L4 9l5 4v-3z"
-			/>
-		</svg>
-	</button>
-	<button class="button" on:click={redoColorScheme}>
-		<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="w-8 fill-black">
-			<path
-				d="M9 18h3v-2H9c-1.654 0-3-1.346-3-3s1.346-3 3-3h6v3l5-4-5-4v3H9c-2.757 0-5 2.243-5 5s2.243 5 5 5z"
-			/>
-		</svg>
-	</button>
-	<div class="flex-1 flex -mx-2">
+<menu class="fixed bottom-4 left-4 right-4 h-64 p-4 flex border-3 rounded-lg bg-white border-black">
+	<div class=" mr-4 flex gap-x-4">
+		<button class="button" on:click={addColorScheme}>
+			<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="w-8 fill-black">
+				<path
+					d="M10 11H7.101l.001-.009a4.956 4.956 0 0 1 .752-1.787 5.054 5.054 0 0 1 2.2-1.811c.302-.128.617-.226.938-.291a5.078 5.078 0 0 1 2.018 0 4.978 4.978 0 0 1 2.525 1.361l1.416-1.412a7.036 7.036 0 0 0-2.224-1.501 6.921 6.921 0 0 0-1.315-.408 7.079 7.079 0 0 0-2.819 0 6.94 6.94 0 0 0-1.316.409 7.04 7.04 0 0 0-3.08 2.534 6.978 6.978 0 0 0-1.054 2.505c-.028.135-.043.273-.063.41H2l4 4 4-4zm4 2h2.899l-.001.008a4.976 4.976 0 0 1-2.103 3.138 4.943 4.943 0 0 1-1.787.752 5.073 5.073 0 0 1-2.017 0 4.956 4.956 0 0 1-1.787-.752 5.072 5.072 0 0 1-.74-.61L7.05 16.95a7.032 7.032 0 0 0 2.225 1.5c.424.18.867.317 1.315.408a7.07 7.07 0 0 0 2.818 0 7.031 7.031 0 0 0 4.395-2.945 6.974 6.974 0 0 0 1.053-2.503c.027-.135.043-.273.063-.41H22l-4-4-4 4z"
+				/>
+			</svg>
+		</button>
+		<button class="button" on:click={undoColorScheme}>
+			<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="w-8 fill-black">
+				<path
+					d="M9 10h6c1.654 0 3 1.346 3 3s-1.346 3-3 3h-3v2h3c2.757 0 5-2.243 5-5s-2.243-5-5-5H9V5L4 9l5 4v-3z"
+				/>
+			</svg>
+		</button>
+		<button class="button" on:click={redoColorScheme}>
+			<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="w-8 fill-black">
+				<path
+					d="M9 18h3v-2H9c-1.654 0-3-1.346-3-3s1.346-3 3-3h6v3l5-4-5-4v3H9c-2.757 0-5 2.243-5 5s2.243 5 5 5z"
+				/>
+			</svg>
+		</button>
+	</div>
+	<div class="flex-1 flex gap-4">
 		{#each colorSchemes[colorSchemeIndex] as color, index (color.id)}
 			<div
-				class="w-full p-8 mx-2 flex flex-col justify-between items-center border-3 rounded-lg border-black transition-colors duration-150"
+				class="flex-1 p-8 flex flex-col justify-between items-center border-3 rounded-lg border-black transition-colors duration-150"
 				style={`background-color: ${color.hex};`}
+				in:receive={{ key: color.id }}
+				out:send={{ key: color.id }}
+				animate:flip={{ duration: 150 }}
 			>
 				<div>
 					{#if colorSchemes[colorSchemeIndex].length > MIN_COLOR_SCHEME_SIZE}
@@ -195,7 +217,7 @@
 		{/each}
 	</div>
 	{#if colorSchemes[colorSchemeIndex].length < MAX_COLOR_SCHEME_SIZE}
-		<button class="button" on:click={addColor}>
+		<button class="button ml-4" on:click={addColor} transition:slide={{ duration: 150, axis: 'x' }}>
 			<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="w-8 fill-black">
 				<path d="M19 11h-6V5h-2v6H5v2h6v6h2v-6h6z" />
 			</svg>
