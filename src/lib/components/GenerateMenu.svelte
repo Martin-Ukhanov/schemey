abaafbfb3c9b45eb587596f354e664472fad1b51
Martin-Ukhanov <martin.ukhanov@gmail.com>
2023-08-10 01:11:15 -0400

    Upgraded UI

--- src/lib/components/GenerateMenu.svelte
+++ src/lib/components/GenerateMenu.svelte
@@ -1,8 +1,7 @@
 <script lang="ts">
 	import { goto } from '$app/navigation';
 	import uniqid from 'uniqid';
-	import { generateColorScheme, colorSchemeToSlug } from '$lib/utils';
-	import { slide } from 'svelte/transition';
+	import { generateColorScheme, colorSchemeToSlug, contrastingColor } from '$lib/utils';
 
 	type Color = {
 		id: string;
@@ -105,41 +104,101 @@
 	}
 </script>
 
-<menu class="fixed bottom-4 left-4 right-4 p-4 flex gap-4 rounded-lg bg-white">
-	<button class="button" on:click={addColorScheme}>Generate</button>
-	<button class="button" on:click={undoColorScheme}>Undo</button>
-	<button class="button" on:click={redoColorScheme}>Redo</button>
-	<div class="flex">
+<menu
+	class="fixed bottom-4 left-4 right-4 h-64 p-4 flex gap-4 border-3 rounded-lg bg-white border-black"
+>
+	<button class="button" on:click={addColorScheme}>
+		<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="w-8 fill-black">
+			<path
+				d="M10 11H7.101l.001-.009a4.956 4.956 0 0 1 .752-1.787 5.054 5.054 0 0 1 2.2-1.811c.302-.128.617-.226.938-.291a5.078 5.078 0 0 1 2.018 0 4.978 4.978 0 0 1 2.525 1.361l1.416-1.412a7.036 7.036 0 0 0-2.224-1.501 6.921 6.921 0 0 0-1.315-.408 7.079 7.079 0 0 0-2.819 0 6.94 6.94 0 0 0-1.316.409 7.04 7.04 0 0 0-3.08 2.534 6.978 6.978 0 0 0-1.054 2.505c-.028.135-.043.273-.063.41H2l4 4 4-4zm4 2h2.899l-.001.008a4.976 4.976 0 0 1-2.103 3.138 4.943 4.943 0 0 1-1.787.752 5.073 5.073 0 0 1-2.017 0 4.956 4.956 0 0 1-1.787-.752 5.072 5.072 0 0 1-.74-.61L7.05 16.95a7.032 7.032 0 0 0 2.225 1.5c.424.18.867.317 1.315.408a7.07 7.07 0 0 0 2.818 0 7.031 7.031 0 0 0 4.395-2.945 6.974 6.974 0 0 0 1.053-2.503c.027-.135.043-.273.063-.41H22l-4-4-4 4z"
+			/>
+		</svg>
+	</button>
+	<button class="button" on:click={undoColorScheme}>
+		<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="w-8 fill-black">
+			<path
+				d="M9 10h6c1.654 0 3 1.346 3 3s-1.346 3-3 3h-3v2h3c2.757 0 5-2.243 5-5s-2.243-5-5-5H9V5L4 9l5 4v-3z"
+			/>
+		</svg>
+	</button>
+	<button class="button" on:click={redoColorScheme}>
+		<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="w-8 fill-black">
+			<path
+				d="M9 18h3v-2H9c-1.654 0-3-1.346-3-3s1.346-3 3-3h6v3l5-4-5-4v3H9c-2.757 0-5 2.243-5 5s2.243 5 5 5z"
+			/>
+		</svg>
+	</button>
+	<div class="flex-1 flex -mx-2">
 		{#each colorSchemes[colorSchemeIndex] as color, index (color.id)}
 			<div
-				class="p-8 mx-2 rounded-lg"
+				class="w-full p-8 mx-2 flex flex-col justify-between items-center border-3 rounded-lg border-black transition-colors duration-150"
 				style={`background-color: ${color.hex};`}
-				transition:slide={{ duration: 300, axis: 'x' }}
 			>
-				{#if colorSchemes[colorSchemeIndex].length > MIN_COLOR_SCHEME_SIZE}
+				<div>
+					{#if colorSchemes[colorSchemeIndex].length > MIN_COLOR_SCHEME_SIZE}
+						<button
+							class={contrastingColor(color.hex) === '#000000'
+								? 'button-transparent-black'
+								: 'button-transparent-white'}
+							style={`color: ${contrastingColor(color.hex)}`}
+							on:click={() => {
+								removeColor(index);
+							}}
+						>
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								viewBox="0 0 24 24"
+								class="w-8"
+								style={`fill: ${contrastingColor(color.hex)}`}
+							>
+								<path
+									d="M21 5a2 2 0 0 0-2-2H5a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V5zm-4.793 9.793-1.414 1.414L12 13.414l-2.793 2.793-1.414-1.414L10.586 12 7.793 9.207l1.414-1.414L12 10.586l2.793-2.793 1.414 1.414L13.414 12l2.793 2.793z"
+								/>
+							</svg>
+						</button>
+					{/if}
 					<button
-						class="button"
+						class={contrastingColor(color.hex) === '#000000'
+							? 'button-transparent-black'
+							: 'button-transparent-white'}
 						on:click={() => {
-							removeColor(index);
+							toggleLockedColor(index);
 						}}
 					>
-						X
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							viewBox="0 0 24 24"
+							class="w-8"
+							style={`fill: ${contrastingColor(color.hex)}`}
+						>
+							{#if colorSchemes[colorSchemeIndex][index].locked}
+								<path
+									d="M20 12c0-1.103-.897-2-2-2h-1V7c0-2.757-2.243-5-5-5S7 4.243 7 7v3H6c-1.103 0-2 .897-2 2v8c0 1.103.897 2 2 2h12c1.103 0 2-.897 2-2v-8zM9 7c0-1.654 1.346-3 3-3s3 1.346 3 3v3H9V7z"
+								/>
+							{:else}
+								<path
+									d="M17 8V7c0-2.757-2.243-5-5-5S7 4.243 7 7v3H6c-1.103 0-2 .897-2 2v8c0 1.103.897 2 2 2h12c1.103 0 2-.897 2-2v-8c0-1.103-.897-2-2-2H9V7c0-1.654 1.346-3 3-3s3 1.346 3 3v1h2z"
+								/>
+							{/if}
+						</svg>
 					</button>
-				{/if}
+				</div>
 				<button
-					class="button"
-					on:click={() => {
-						toggleLockedColor(index);
-					}}
+					class={contrastingColor(color.hex) === '#000000'
+						? 'button-transparent-black'
+						: 'button-transparent-white'}
+					style={`color: ${contrastingColor(color.hex)}`}
 				>
-					{colorSchemes[colorSchemeIndex][index].locked ? 'Unlock' : 'Lock'}
+					<span class="text-lg font-bold">{color.hex}</span>
 				</button>
 			</div>
 		{/each}
 	</div>
 	{#if colorSchemes[colorSchemeIndex].length < MAX_COLOR_SCHEME_SIZE}
-		<button class="button" on:click={addColor} transition:slide={{ duration: 300, axis: 'x' }}>
-			+
+		<button class="button" on:click={addColor}>
+			<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="w-8 fill-black">
+				<path d="M19 11h-6V5h-2v6H5v2h6v6h2v-6h6z" />
+			</svg>
 		</button>
 	{/if}
 </menu>
