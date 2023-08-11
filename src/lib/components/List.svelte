8f78b6686e1b3c94dd31fb8cc9ca21d4712f3584
Martin-Ukhanov <martin.ukhanov@gmail.com>
2023-08-11 19:23:08 -0400

    Created List component

---
+++ src/lib/components/List.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	export let items: any[];
+	export let selectedItem: any;
+</script>
+
+<div class="flex flex-col overflow-y-auto">
+	{#each items as item}
+		<button
+			class="button mr-2 justify-start border-none text-left"
+			class:brightness-80={item === selectedItem}
+			on:click={() => {
+				selectedItem = item;
+			}}
+		>
+			{item}
+		</button>
+	{/each}
+</div>
