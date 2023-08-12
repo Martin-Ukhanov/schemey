492d466ad241fd5422bc4781f26f041cd333f8c2
Martin-Ukhanov <martin.ukhanov@gmail.com>
2023-08-12 13:01:01 -0400

    Disable undo and redo buttons

--- src/lib/components/GenerateMenu.svelte
+++ src/lib/components/GenerateMenu.svelte
@@ -240,14 +240,18 @@
 				</svg>
 			</button>
 			<div class="flex gap-x-4">
-				<button class="button flex-1" on:click={undoColorScheme}>
+				<button
+					class="button flex-1"
+					on:click={undoColorScheme}
+					disabled={colorSchemes.length === 1 || colorSchemeIndex === colorSchemes.length - 1}
+				>
 					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="w-8 fill-black">
 						<path
 							d="M9 10h6c1.654 0 3 1.346 3 3s-1.346 3-3 3h-3v2h3c2.757 0 5-2.243 5-5s-2.243-5-5-5H9V5L4 9l5 4v-3z"
 						/>
 					</svg>
 				</button>
-				<button class="button flex-1" on:click={redoColorScheme}>
+				<button class="button flex-1" on:click={redoColorScheme} disabled={colorSchemeIndex === 0}>
 					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="w-8 fill-black">
 						<path
 							d="M9 18h3v-2H9c-1.654 0-3-1.346-3-3s1.346-3 3-3h6v3l5-4-5-4v3H9c-2.757 0-5 2.243-5 5s2.243 5 5 5z"
