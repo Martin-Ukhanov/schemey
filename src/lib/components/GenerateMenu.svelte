51d3f37a1c5358435cd6f35bc510ff1da87ab56e
Martin-Ukhanov <martin.ukhanov@gmail.com>
2023-08-11 21:31:37 -0400

    Added bokmark buttons for colors and scheme

--- src/lib/components/GenerateMenu.svelte
+++ src/lib/components/GenerateMenu.svelte
@@ -140,6 +140,16 @@
 	});
 </script>
 
+<!-- <menu class="fixed top-0 left-0 right-0 p-4 border-b-3 bg-white border-black">
+	<button class="button flex-1" on:click={addColorScheme}>
+		<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="w-8 fill-black">
+			<path
+				d="M10 11H7.101l.001-.009a4.956 4.956 0 0 1 .752-1.787 5.054 5.054 0 0 1 2.2-1.811c.302-.128.617-.226.938-.291a5.078 5.078 0 0 1 2.018 0 4.978 4.978 0 0 1 2.525 1.361l1.416-1.412a7.036 7.036 0 0 0-2.224-1.501 6.921 6.921 0 0 0-1.315-.408 7.079 7.079 0 0 0-2.819 0 6.94 6.94 0 0 0-1.316.409 7.04 7.04 0 0 0-3.08 2.534 6.978 6.978 0 0 0-1.054 2.505c-.028.135-.043.273-.063.41H2l4 4 4-4zm4 2h2.899l-.001.008a4.976 4.976 0 0 1-2.103 3.138 4.943 4.943 0 0 1-1.787.752 5.073 5.073 0 0 1-2.017 0 4.956 4.956 0 0 1-1.787-.752 5.072 5.072 0 0 1-.74-.61L7.05 16.95a7.032 7.032 0 0 0 2.225 1.5c.424.18.867.317 1.315.408a7.07 7.07 0 0 0 2.818 0 7.031 7.031 0 0 0 4.395-2.945 6.974 6.974 0 0 0 1.053-2.503c.027-.135.043-.273.063-.41H22l-4-4-4 4z"
+			/>
+		</svg>
+	</button>
+</menu> -->
+
 <menu
 	class="fixed bottom-0 left-0 right-0 h-96 p-4 flex border-t-3 bg-white border-black transition-transform duration-150"
 	class:translate-y-full={!menuOpen}
@@ -161,7 +171,14 @@
 			/>
 		</svg>
 	</button>
-	<div class=" mr-4 flex flex-col gap-y-4">
+	<div class="w-36 mr-4 flex flex-col gap-y-4">
+		<button class="button">
+			<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="w-8 fill-black">
+				<path
+					d="M18 2H6c-1.103 0-2 .897-2 2v18l8-4.572L20 22V4c0-1.103-.897-2-2-2zm0 16.553-6-3.428-6 3.428V4h12v14.553z"
+				/>
+			</svg>
+		</button>
 		<button
 			class="button font-bold"
 			on:click={() => {
@@ -178,14 +195,14 @@
 			</svg>
 		</button>
 		<div class="flex gap-x-4">
-			<button class="button" on:click={undoColorScheme}>
+			<button class="button flex-1" on:click={undoColorScheme}>
 				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="w-8 fill-black">
 					<path
 						d="M9 10h6c1.654 0 3 1.346 3 3s-1.346 3-3 3h-3v2h3c2.757 0 5-2.243 5-5s-2.243-5-5-5H9V5L4 9l5 4v-3z"
 					/>
 				</svg>
 			</button>
-			<button class="button" on:click={redoColorScheme}>
+			<button class="button flex-1" on:click={redoColorScheme}>
 				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="w-8 fill-black">
 					<path
 						d="M9 18h3v-2H9c-1.654 0-3-1.346-3-3s1.346-3 3-3h6v3l5-4-5-4v3H9c-2.757 0-5 2.243-5 5s2.243 5 5 5z"
@@ -210,7 +227,6 @@
 							class={contrast === '#000000'
 								? 'button-transparent-black'
 								: 'button-transparent-white'}
-							style={`color: ${contrast}`}
 							on:click={() => {
 								removeColor(index);
 							}}
@@ -227,6 +243,20 @@
 							</svg>
 						</button>
 					{/if}
+					<button
+						class={contrast === '#000000' ? 'button-transparent-black' : 'button-transparent-white'}
+					>
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							viewBox="0 0 24 24"
+							class="w-8"
+							style={`fill: ${contrast}`}
+						>
+							<path
+								d="M18 2H6c-1.103 0-2 .897-2 2v18l8-4.572L20 22V4c0-1.103-.897-2-2-2zm0 16.553-6-3.428-6 3.428V4h12v14.553z"
+							/>
+						</svg>
+					</button>
 					<button
 						class={contrast === '#000000' ? 'button-transparent-black' : 'button-transparent-white'}
 						on:click={() => {
