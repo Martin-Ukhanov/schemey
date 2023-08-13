4cd69b95b968e6bc293256a04ff62c2e271d4d12
Martin-Ukhanov <martin.ukhanov@gmail.com>
2023-08-12 23:04:11 -0400

    Decreased modal width

--- src/lib/components/Modal.svelte
+++ src/lib/components/Modal.svelte
@@ -11,7 +11,7 @@
 		transition:fade={{ duration: 200 }}
 	/>
 	<div
-		class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-full max-w-96"
+		class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-full max-w-80"
 		transition:scale={{ duration: 200 }}
 	>
 		<div class="h-80 p-4 mx-4 flex flex-col gap-y-4 border-3 rounded-md bg-white border-black">
