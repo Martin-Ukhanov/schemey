e01e86cb3fdbf8f99a7f0c9294cba6b0646fe795
Martin-Ukhanov <martin.ukhanov@gmail.com>
2023-08-12 23:17:13 -0400

    Updated font to Poppins

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,7 @@ export default {
 	theme: {
 		extend: {
 			fontFamily: {
-				poppins: ['Poppins', 'sans-serif']
+				nunito: ['Nunito', 'sans-serif']
 			},
 			maxWidth: (theme) => ({
 				...theme('spacing')
