4e54ff22ad22775ce9fb767e9990691e9c68e682
Martin-Ukhanov <martin.ukhanov@gmail.com>
2023-08-16 11:17:20 -0400

    Created cursor

--- src/lib/components/ColorPicker.svelte
+++ src/lib/components/ColorPicker.svelte
@@ -1,12 +1,33 @@
 <script lang="ts">
+	import { browser } from '$app/environment';
 	import convert from 'color-convert';
 
 	export let hex: string;
 
+	let canvas: HTMLButtonElement;
 	let width: number;
 	let height: number;
 
 	let [h, s, v] = convert.hex.hsv(hex);
+
+	function pickColorMouse(e: MouseEvent): void {
+		const rect = canvas.getBoundingClientRect();
+
+		s = Math.min(Math.max(Math.round(((e.clientX - rect.left) / width) * 100), 0), 100);
+		v = Math.min(Math.max(100 - Math.round(((e.clientY - rect.top) / height) * 100), 0), 100);
+
+		if (browser) {
+			document.onmousemove = (e) => {
+				s = Math.min(Math.max(Math.round(((e.clientX - rect.left) / width) * 100), 0), 100);
+				v = Math.min(Math.max(100 - Math.round(((e.clientY - rect.top) / height) * 100), 0), 100);
+			};
+
+			document.onmouseup = () => {
+				document.onmousemove = null;
+				document.onmouseup = null;
+			};
+		}
+	}
 
 	$: hex = '#' + convert.hsv.hex([h, s, v]);
 </script>
@@ -15,16 +36,14 @@
 	class="relative w-32 h-32"
 	style={`background: linear-gradient(#ffffff00, #000000ff),
   linear-gradient(0.25turn, #ffffffff, #00000000), hsl(${h}, 100%, 50%);`}
+	bind:this={canvas}
 	bind:clientWidth={width}
 	bind:clientHeight={height}
-	on:click={(e) => {
-		s = Math.round((e.offsetX / width) * 100);
-		v = 100 - Math.round((e.offsetY / height) * 100);
-	}}
+	on:mousedown={pickColorMouse}
 >
 	<div
-		class="absolute -translate-x-1/2 translate-y-1/2 w-2 h-2 bg-white"
-		style={`bottom: ${v}%; left: ${s}%`}
+		class="absolute -translate-x-1/2 translate-y-1/2 w-5 h-5 border-4 rounded-full border-white"
+		style={`bottom: ${v}%; left: ${s}%; background-color: ${hex}`}
 	/>
 </button>
 <div class="w-8 h-8" style={`background: ${hex};`} />
