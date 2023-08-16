3d0ae4e40f34714c5b56254d25a590b47e8b7591
Martin-Ukhanov <martin.ukhanov@gmail.com>
2023-08-16 00:43:53 -0400

    Utilize color convert

--- src/lib/utils/index.ts
+++ src/lib/utils/index.ts
@@ -1,19 +1,13 @@
-import iwanthue, { type RGBColor, type ColorSpace } from 'iwanthue';
+import convert from 'color-convert';
+import type { RGB } from 'color-convert/conversions';
+import iwanthue, { type ColorSpace } from 'iwanthue';
 
-function hexToRgb(hex: string): RGBColor {
-	const r = parseInt(hex.slice(1, 3), 16);
-	const g = parseInt(hex.slice(3, 5), 16);
-	const b = parseInt(hex.slice(5, 7), 16);
-
-	return [r, g, b];
-}
-
-function rgbLuminance(rgb: RGBColor): number {
+function rgbLuminance(rgb: RGB): number {
 	return (0.299 * rgb[0] + 0.587 * rgb[1] + 0.114 * rgb[2]) / 255;
 }
 
 export function contrastingColor(hex: string): '#000000' | '#ffffff' {
-	return rgbLuminance(hexToRgb(hex)) <= 0.5 ? '#ffffff' : '#000000';
+	return rgbLuminance(convert.hex.rgb(hex)) <= 0.5 ? '#ffffff' : '#000000';
 }
 
 type ColorSpacePresets = {
