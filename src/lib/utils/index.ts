a261267816e072c5990652814a565a761fb00676
Martin-Ukhanov <martin.ukhanov@gmail.com>
2023-08-17 19:11:03 -0400

    Moved color space presets to store

--- src/lib/utils/index.ts
+++ src/lib/utils/index.ts
@@ -9,27 +9,6 @@ export function contrastingColor(hex: string): '#000000' | '#ffffff' {
 	return Color(hex).isLight() ? '#000000' : '#ffffff';
 }
 
-type ColorSpacePresets = {
-	[key: string]: ColorSpace;
-};
-
-export const COLOR_SPACE_PRESETS: ColorSpacePresets = {
-	all: [0, 360, 0, 100, 0, 100],
-	light: [0, 360, 15, 40, 70, 100],
-	dark: [0, 360, 8, 40, 7, 40],
-	pastel: [0, 360, 0, 30, 70, 100],
-	shades: [0, 240, 0, 15, 0, 100],
-	intense: [0, 360, 20, 100, 15, 80],
-	red: [330, 20, 10, 100, 35, 100],
-	blue: [220, 260, 8, 80, 0, 50],
-	green: [90, 150, 10, 100, 35, 100],
-	yellow: [60, 90, 10, 100, 35, 100],
-	teal: [150, 200, 0, 100, 35, 100],
-	purple: [290, 330, 0, 100, 0, 40],
-	earth: [20, 60, 20, 50, 35, 100],
-	colorblind: [0, 360, 40, 70, 15, 85]
-};
-
 export function generateColorScheme(size: number, colorSpace: ColorSpace): string[] {
 	return iwanthue(size, {
 		clustering: 'force-vector',
