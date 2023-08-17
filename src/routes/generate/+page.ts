628ee9b4d6e0575a75da1275310a1bd78b2dab3b
Martin-Ukhanov <martin.ukhanov@gmail.com>
2023-08-17 19:37:34 -0400

    Hard coded color space

--- src/routes/generate/+page.ts
+++ src/routes/generate/+page.ts
@@ -1,10 +1,10 @@
 import { redirect } from '@sveltejs/kit';
-import { generateColorScheme, colorSchemeToSlug, COLOR_SPACE_PRESETS } from '$lib/utils';
+import { generateColorScheme, colorSchemeToSlug } from '$lib/utils';
 import type { PageLoad } from './$types';
 
 export const load = (() => {
 	throw redirect(
 		308,
-		`/generate/${colorSchemeToSlug(generateColorScheme(3, COLOR_SPACE_PRESETS['all']))}`
+		`/generate/${colorSchemeToSlug(generateColorScheme(3, [0, 360, 0, 100, 0, 100]))}`
 	);
 }) satisfies PageLoad;
