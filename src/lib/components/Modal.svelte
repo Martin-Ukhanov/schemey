eb44b316704ef32379fa3fb2132e416e341f99a2
Martin-Ukhanov <martin.ukhanov@gmail.com>
2023-08-17 20:42:19 -0400

    Smaller title

--- src/lib/components/Modal.svelte
+++ src/lib/components/Modal.svelte
@@ -31,7 +31,7 @@
 					<XIcon />
 				</button>
 
-				<div class="text-center text-xl font-bold uppercase text-black">{title}</div>
+				<div class="text-center text-lg uppercase text-black">{title}</div>
 			</div>
 
 			<slot />
