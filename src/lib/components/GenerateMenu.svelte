99a05d63f1eedc824bc091d3e6e26651f02a9e89
Martin-Ukhanov <martin.ukhanov@gmail.com>
2023-08-20 19:02:16 -0400

    Increased max height of bottom menu

--- src/lib/components/GenerateMenu.svelte
+++ src/lib/components/GenerateMenu.svelte
@@ -248,7 +248,7 @@
 {/if}
 
 <menu
-	class="fixed bottom-0 w-full h-[291px] min-h-[291px] max-h-[calc(100%-theme(height.40)-theme(borderWidth.3))] py-4 border-t-3 z-10 bg-white border-black transition-transform duration-300"
+	class="fixed bottom-0 w-full h-[291px] min-h-[291px] max-h-[calc(100%-theme(height.32))] py-4 border-t-3 z-10 bg-white border-black transition-transform duration-300"
 	class:translate-y-full={!menuOpen}
 	bind:this={menuElement}
 	bind:clientWidth={menuWidth}
