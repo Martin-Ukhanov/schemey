4aaf0c74d5e1777c34ff8ee64dd0b6faa9e132e5
Martin-Ukhanov <martin.ukhanov@gmail.com>
2023-08-20 19:26:08 -0400

    Updated look

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,7 +1,7 @@
-<header class="h-16 p-2 flex justify-between items-center border-b-3 border-black">
-	<a href="/" class="text-3xl">Web Colors</a>
+<header class="h-16 p-2 flex justify-between items-center border-b-2 border-black">
+	<a href="/" class="text-2xl">ColorSchemes.io</a>
 	<nav class="flex gap-x-2">
-		<button class="button">Sign In</button>
+		<button class="button-transparent-black">Sign In</button>
 		<button class="button">Sign Up</button>
 	</nav>
 </header>
