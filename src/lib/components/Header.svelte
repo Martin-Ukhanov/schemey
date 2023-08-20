3287d50ac45bcc44b61ca08f064b0f9d959cdd83
Martin-Ukhanov <martin.ukhanov@gmail.com>
2023-08-19 23:53:06 -0400

    Created Header component

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,7 @@
+<header class="h-16 p-2 flex justify-between items-center border-b-3 border-black">
+	<a href="/" class="text-3xl">Web Colors</a>
+	<nav class="flex gap-x-2">
+		<button class="button">Sign In</button>
+		<button class="button">Sign Up</button>
+	</nav>
+</header>
