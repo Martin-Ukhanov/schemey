12e37377a2c439a4d0ece93a085a6419b7da7fb6
Martin-Ukhanov <martin.ukhanov@gmail.com>
2023-08-21 19:25:52 -0400

    Logo button

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,5 +1,5 @@
 <header class="h-16 p-2 flex justify-between items-center border-b-2 border-black">
-	<a href="/" class="text-2xl">ColorSchemes.io</a>
+	<a href="/" class="button-transparent-black text-2xl">ColorSchemes.io</a>
 	<nav class="flex gap-x-2">
 		<button class="button-transparent-black">Sign In</button>
 		<button class="button">Sign Up</button>
