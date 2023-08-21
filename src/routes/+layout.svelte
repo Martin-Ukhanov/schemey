5f2b3f622da379bdba66eacb9625db11b7688a6e
Martin-Ukhanov <martin.ukhanov@gmail.com>
2023-08-21 01:41:45 -0400

    Added Tooltip component

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import '../app.css';
 	import Header from '$lib/components/Header.svelte';
+	import Tooltip from '$lib/components/Tooltip.svelte';
 	import Notifications from '$lib/components/Notifications.svelte';
 </script>
 
@@ -9,5 +10,7 @@
 <main class="min-h-[calc(100dvh-theme(height.16))] flex flex-col">
 	<slot />
 </main>
+
+<Tooltip />
 
 <Notifications />
