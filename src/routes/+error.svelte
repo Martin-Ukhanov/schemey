af1062d66a46000b9c1a7dfb1834f94107a64698
Martin-Ukhanov <martin.ukhanov@gmail.com>
2023-08-23 13:50:17 -0400

    Styled

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -2,4 +2,7 @@
 	import { page } from '$app/stores';
 </script>
 
-<h1 class="text-red-500">{$page.status}: {$page.error?.message}</h1>
+<div class="flex-1 flex flex-col justify-center items-center gap-y-2">
+	<h1 class="text-6xl">{$page.status}</h1>
+	<p>{$page.error?.message}</p>
+</div>
