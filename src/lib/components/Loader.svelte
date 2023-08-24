66a6f00e4d6024f5ed59b418d5d3283b5369ab21
Martin-Ukhanov <martin.ukhanov@gmail.com>
2023-08-24 00:58:05 -0400

    Updated track color

--- src/lib/components/Loader.svelte
+++ src/lib/components/Loader.svelte
@@ -1,1 +1,1 @@
-<div class="absolute w-5 h-5 border-4 border-gray-400 border-t-black rounded-full animate-spin" />
+<div class="absolute w-5 h-5 border-4 border-black/20 border-t-black rounded-full animate-spin" />
