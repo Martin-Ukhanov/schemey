479a1492d1e561949bdb93b29019acec4ad2ff2e
Martin-Ukhanov <martin.ukhanov@gmail.com>
2023-08-24 00:22:59 -0400

    Added menu button

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -2,15 +2,16 @@
 	import { page } from '$app/stores';
 	import { enhance, applyAction } from '$app/forms';
 	import { addNotification } from '$lib/stores/notifications';
-	import Loader from './Loader.svelte';
 	import { signInModalOpen, signUpModalOpen } from '$lib/stores/auth';
+	import Loader from './Loader.svelte';
+	import MenuIcon from './icons/MenuIcon.svelte';
 
 	let loading = false;
 </script>
 
 <header class="h-16 p-2 flex justify-between items-center border-b-2 border-black">
-	<a href="/" class="button-transparent-black text-2xl font-bold">ColorSchemes.io</a>
-	<nav class="flex gap-x-2">
+	<a href="/" class="button-transparent-black text-xl sm:text-2xl font-bold">ColorSchemes.io</a>
+	<nav class="hidden sm:flex gap-x-2">
 		{#if $page.data.session}
 			<form
 				method="post"
@@ -54,4 +55,7 @@
 			>
 		{/if}
 	</nav>
+	<button class="button-transparent-black block sm:hidden">
+		<MenuIcon />
+	</button>
 </header>
