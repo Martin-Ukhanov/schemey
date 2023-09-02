61fa2d02aa5dc72c6da9e7d7bed4c491010361ce
Martin-Ukhanov <martin.ukhanov@gmail.com>
2023-09-01 21:15:21 -0400

    Increased height

--- src/lib/components/List.svelte
+++ src/lib/components/List.svelte
@@ -7,7 +7,7 @@
 	const dispatch = createEventDispatcher();
 </script>
 
-<div class="max-h-72 p-2 flex flex-col overflow-y-auto border-2 rounded-md border-black">
+<div class="h-80 p-2 flex flex-col overflow-y-auto border-2 rounded-md border-black">
 	{#each items as item}
 		<button
 			class="button justify-start border-none text-left"
