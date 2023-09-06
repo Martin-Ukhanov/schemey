89e3559a5808344e91f306252919145c59c52851
Martin-Ukhanov <martin.ukhanov@gmail.com>
2023-09-05 23:52:39 -0400

    No brightness change on hover for saved colors

--- src/lib/components/ColorPicker.svelte
+++ src/lib/components/ColorPicker.svelte
@@ -114,7 +114,7 @@
 			{:else}
 				{#each $savedColors as color (color)}
 					<button
-						class="button aspect-square"
+						class="button aspect-square !brightness-100"
 						style={`background-color: ${color};`}
 						use:showTooltip={{ position: 'top', message: color }}
 						on:click={() => {
