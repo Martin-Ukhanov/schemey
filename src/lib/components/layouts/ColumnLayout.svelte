b91a0e23f2b0f22e84b27eb02b03e86f2397ede2
Martin-Ukhanov <martin.ukhanov@gmail.com>
2023-09-07 19:54:53 -0400

    Created ColumnLayout component

---
+++ src/lib/components/layouts/ColumnLayout.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import { scale } from 'svelte/transition';
+	import { flip } from 'svelte/animate';
+
+	export let colorScheme: { id: string; hex: string }[];
+</script>
+
+<div class="flex-1 flex flex-col">
+	{#each colorScheme as color (color.id)}
+		<div
+			in:scale={{ duration: 300 }}
+			out:scale={{ duration: 300 }}
+			animate:flip={{ duration: 300 }}
+			class="flex-1"
+			style={`background-color: ${color.hex}`}
+		/>
+	{/each}
+</div>
