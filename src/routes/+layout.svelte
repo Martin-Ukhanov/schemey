5e349f0a3484c2212fccd58a7c847a1467289e1f
Martin-Ukhanov <martin.ukhanov@gmail.com>
2023-09-07 18:52:47 -0400

    Relative main

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -37,7 +37,7 @@
 
 <Header />
 
-<main class="min-h-[calc(100dvh-theme(height.16))] flex flex-col">
+<main class="relative min-h-[calc(100dvh-theme(height.16))] flex flex-col">
 	<slot />
 </main>
 
