dc869fa9c08686981edd14577677fda5b4e0136d
Martin-Ukhanov <martin.ukhanov@gmail.com>
2023-09-07 19:04:47 -0400

    Decereased min height

--- src/lib/components/GenerateMenu.svelte
+++ src/lib/components/GenerateMenu.svelte
@@ -388,7 +388,7 @@
 {/if}
 
 <menu
-	class="fixed bottom-0 w-full h-[291px] min-h-[291px] max-h-[calc(100%-7.5rem)] py-4 border-t-2 z-10 bg-white border-black transition-transform duration-300"
+	class="fixed bottom-0 w-full h-[266px] min-h-[266px] max-h-[calc(100%-7.5rem)] py-4 border-t-2 z-10 bg-white border-black transition-transform duration-300"
 	class:translate-y-full={!isMenuOpen}
 	bind:this={menuElement}
 	bind:clientWidth={menuWidth}
