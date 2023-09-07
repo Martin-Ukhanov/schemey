9f60d55af829c1efa32483dbe3fe32fbbf07d4a2
Martin-Ukhanov <martin.ukhanov@gmail.com>
2023-09-07 18:54:06 -0400

    Created SimpleLayout component

---
+++ src/lib/components/SimpleLayout.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	export let colorScheme: string[];
+</script>
+
+<div class="flex-1 flex">
+	{#each colorScheme as color}
+		<div class="flex-1" style={`background-color: ${color}`} />
+	{/each}
+</div>
