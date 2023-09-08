b0308076bdd2218553e17f898445963908de9ee8
Martin-Ukhanov <martin.ukhanov@gmail.com>
2023-09-07 21:17:02 -0400

    Notifications position higher

--- src/lib/components/Notifications.svelte
+++ src/lib/components/Notifications.svelte
@@ -13,7 +13,7 @@
 </script>
 
 <div
-	class="fixed top-20 left-4 right-4 flex flex-col items-center gap-y-4 z-10 pointer-events-none"
+	class="fixed top-10 left-4 right-4 flex flex-col items-center gap-y-4 z-10 pointer-events-none"
 >
 	{#each $notifications as notification (notification.id)}
 		{@const contrastColor = contrastingColor(notification.color)}
