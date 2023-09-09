f485eeb7ec3ef7a396ac0bbc5e5fd578361e8ea2
Martin-Ukhanov <martin.ukhanov@gmail.com>
2023-09-09 14:04:46 -0400

    Created ContrastLayout component

---
+++ src/lib/components/layouts/ContrastLayout.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	import { scale } from 'svelte/transition';
+	import { flip } from 'svelte/animate';
+
+	export let colorScheme: { id: string; hex: string }[];
+</script>
+
+<div class="flex-1 flex flex-col md:flex-row">
+	{#each colorScheme as color, index1 (color.id)}
+		<div
+			class="flex-1 p-4 flex flex-row md:flex-col gap-4"
+			style={`background-color: ${color.hex}`}
+			in:scale={{ duration: 200 }}
+			out:scale={{ duration: 200 }}
+			animate:flip={{ duration: 200 }}
+		>
+			{#each colorScheme as color, index2 (color.id)}
+				<div
+					class="flex-1 rounded-full"
+					class:hidden={index1 === index2}
+					style={`background-color: ${color.hex}`}
+					in:scale={{ duration: 200 }}
+					out:scale={{ duration: 200 }}
+					animate:flip={{ duration: 200 }}
+				/>
+			{/each}
+		</div>
+	{/each}
+</div>
