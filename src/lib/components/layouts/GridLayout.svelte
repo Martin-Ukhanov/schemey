566b791d00d217d09f58858680b33a5bc3a0a46c
Martin-Ukhanov <martin.ukhanov@gmail.com>
2023-09-09 19:28:29 -0400

    Created GridLayout component

---
+++ src/lib/components/layouts/GridLayout.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+	import { scale } from 'svelte/transition';
+	import { flip } from 'svelte/animate';
+
+	export let colorScheme: { id: string; hex: string }[];
+
+	let grid: { id: string; hex: string }[][] = [];
+
+	$: if (colorScheme) {
+		grid = [];
+
+		for (let i = 0; i < colorScheme.length; i++) {
+			grid.push(structuredClone(colorScheme));
+
+			colorScheme.push(<{ id: string; hex: string }>colorScheme.shift());
+		}
+	}
+</script>
+
+<div class="flex-1 flex">
+	{#each grid as section (section[0].id)}
+		<div
+			class="flex-1 flex flex-col"
+			in:scale={{ duration: 200 }}
+			out:scale={{ duration: 200 }}
+			animate:flip={{ duration: 200 }}
+		>
+			{#each section as color (color.id)}
+				<div
+					class="flex-1"
+					style={`background-color: ${color.hex}`}
+					in:scale={{ duration: 200 }}
+					out:scale={{ duration: 200 }}
+					animate:flip={{ duration: 200 }}
+				/>
+			{/each}
+		</div>
+	{/each}
+</div>
