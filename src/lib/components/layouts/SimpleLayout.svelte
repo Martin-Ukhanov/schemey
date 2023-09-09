76680e8b010c154b01116d0cb1a400a1a50a0b38
Martin-Ukhanov <martin.ukhanov@gmail.com>
2023-09-09 14:04:35 -0400

    Combined row and column layout into simple layout

--- src/lib/components/layouts/SimpleLayout.svelte
+++ src/lib/components/layouts/SimpleLayout.svelte
@@ -5,14 +5,14 @@
 	export let colorScheme: { id: string; hex: string }[];
 </script>
 
-<div class="flex-1 flex flex-col">
+<div class="flex-1 flex flex-col md:flex-row">
 	{#each colorScheme as color (color.id)}
 		<div
 			in:scale={{ duration: 200 }}
 			out:scale={{ duration: 200 }}
 			animate:flip={{ duration: 200 }}
 			class="flex-1"
-			style={`background-color: ${color.hex};`}
+			style={`background-color: ${color.hex}`}
 		/>
 	{/each}
 </div>
