77929c4196b7d3b73463fc16257665e4b6a2badb
Martin-Ukhanov <martin.ukhanov@gmail.com>
2023-09-13 11:43:30 -0400

    Use colorSpacePresets store

--- src/routes/generate/+page.ts
+++ src/routes/generate/+page.ts
@@ -1,10 +1,12 @@
 import { redirect } from '@sveltejs/kit';
+import { get } from 'svelte/store';
 import { generateColorScheme, colorSchemeToSlug } from '$lib/utils';
+import { colorSpacePresets } from '$lib/stores/colorSpacePresets';
 import type { PageLoad } from './$types';
 
 export const load = (() => {
 	throw redirect(
 		303,
-		`/generate/${colorSchemeToSlug(generateColorScheme(3, [0, 360, 0, 100, 0, 100]))}`
+		`/generate/${colorSchemeToSlug(generateColorScheme(3, get(colorSpacePresets)['all']))}`
 	);
 }) satisfies PageLoad;
