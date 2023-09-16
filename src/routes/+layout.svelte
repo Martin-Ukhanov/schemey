55c95ca6e79a9a42c58eeedbc240ac3402c1f13e
Martin-Ukhanov <martin.ukhanov@gmail.com>
2023-09-15 21:10:30 -0400

    Updated notifications

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -24,10 +24,25 @@
 			}
 		});
 
-		if ($page.url.searchParams.has('signedIn')) {
-			addNotification('Successfully Signed In', 'check');
-		} else if ($page.url.searchParams.has('updatedEmail')) {
-			addNotification('Successfully Updated Email', 'check');
+		if ($page.url.searchParams.has('type')) {
+			const type = $page.url.searchParams.get('type');
+			const success = $page.url.searchParams.get('success');
+
+			if (type === 'magiclink') {
+				if (success === 'true') {
+					addNotification('Successfully Signed In', 'check');
+				} else {
+					addNotification('Sign In Failed', 'x');
+				}
+			} else if (type === 'email_change') {
+				if (success === 'true') {
+					addNotification('Successfully Updated Email', 'check');
+				} else {
+					addNotification('Failed to Update Email', 'x');
+				}
+			}
+		} else if ($page.url.searchParams.has('auth_code_error')) {
+			addNotification('Auth Code Error Occured', 'x');
 		}
 
 		return () => data.subscription.unsubscribe();
