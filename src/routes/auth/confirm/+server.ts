7b83298280ee3ee101e6a7326e51bea2a3b80608
Martin-Ukhanov <martin.ukhanov@gmail.com>
2023-09-15 20:27:31 -0400

    Created auth confirm

---
+++ src/routes/auth/confirm/+server.ts
@@ -0,0 +1,23 @@
+import { redirect } from '@sveltejs/kit';
+import type { RequestHandler } from './$types';
+import type { EmailOtpType } from '@supabase/supabase-js';
+
+export const GET: RequestHandler = async (event) => {
+	const {
+		url,
+		locals: { supabase }
+	} = event;
+	const token_hash = <string>url.searchParams.get('token_hash');
+	const type = <EmailOtpType>url.searchParams.get('type');
+	const next = url.searchParams.get('next') ?? '/';
+
+	if (token_hash && type) {
+		const { error } = await supabase.auth.verifyOtp({ token_hash, type });
+
+		if (!error) {
+			throw redirect(303, `/${next.slice(1)}`);
+		}
+	}
+
+	throw redirect(303, '/');
+};
