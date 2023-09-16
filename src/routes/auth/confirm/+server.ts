6ea1adec1ffff3847a487bad2da0332366aea480
Martin-Ukhanov <martin.ukhanov@gmail.com>
2023-09-15 21:10:48 -0400

    Updated error handling

--- src/routes/auth/confirm/+server.ts
+++ src/routes/auth/confirm/+server.ts
@@ -9,15 +9,16 @@ export const GET: RequestHandler = async (event) => {
 	} = event;
 	const token_hash = <string>url.searchParams.get('token_hash');
 	const type = <EmailOtpType>url.searchParams.get('type');
-	const next = url.searchParams.get('next') ?? '/';
 
 	if (token_hash && type) {
 		const { error } = await supabase.auth.verifyOtp({ token_hash, type });
 
 		if (!error) {
-			throw redirect(303, `/${next.slice(1)}`);
+			throw redirect(303, `/?type=${type}&success=true`);
+		} else {
+			throw redirect(303, `/?type=${type}&success=false`);
 		}
 	}
 
-	throw redirect(303, '/');
+	throw redirect(303, '/?auth_code_error');
 };
