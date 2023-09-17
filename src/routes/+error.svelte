8eb87d9a13a8f045607338467725e053b318b58c
Martin-Ukhanov <martin.ukhanov@gmail.com>
2023-09-17 15:45:30 -0400

    Updated error page

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -3,10 +3,11 @@
 </script>
 
 <svelte:head>
-	<title>Schemey | 404 Not Found</title>
+	<title>Schemey | Error</title>
 </svelte:head>
 
 <div class="flex-1 flex flex-col justify-center items-center gap-y-2">
-	<h1 class="text-6xl">{$page.status}</h1>
-	<p>{$page.error?.message}</p>
+	<h1 class="text-center text-5xl sm:text-7xl font-bold text-primary">{$page.status}</h1>
+
+	<p class="text-center text-lg sm:text-2xl uppercase font-bold">{$page.error?.message}</p>
 </div>
